404d157855b2ee34db80e7625c781503eb1170a5
novaardiansyah <novaardiansyah78@gmail.com>
2022-05-21 14:54:52 +0700

    Custom endpoints for mainCards and animals

---
+++ server/db.js
@@ -0,0 +1,30 @@
+const mainCards = [
+  {
+    title: 'Recently Viewed',
+    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    title: 'Looking for a Gift',
+    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  }
+];
+
+const animals = [
+  {
+    title: '7-Year-Old Puppy',
+    name: 'Bella',
+    image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    rating: 4.5,
+    price: 25.99,
+    estimatedDelivery: '2-3 weeks',
+    description: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.',
+    ],
+    stock: 10,
+    onSale: true,
+  }
+];
+
+module.exports = { mainCards, animals };
